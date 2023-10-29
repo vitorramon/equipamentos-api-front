6127718ab5f71b218c74358fad95aae983204227
vitorramon <rvramon16@gmail.com>
2023-10-29 15:54:11 -0400

    Implementando interface: estilo, formulário, importação de módulos: FormsModule, HttpClientModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{ HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent {
+    //Variável para visibilidade dos botões
+
+    btnCadastro:boolean = true;
+}
